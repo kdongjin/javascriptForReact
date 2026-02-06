98021f9285466336a98a0b2d1b679aa31efd20a0
rlaeogus0911 <rlaeogus0911@nate.com>
2026-02-06 09:31:00 +0900

    [생성] 리액트 생성 및 스타트

--- section01/chapter01/chapter20.js
+++ section01/chapter01/chapter20.js
@@ -1,6 +1,7 @@
 //내장함수(라이브러리) (반복문 => 자기스타일로 변경 반복문)
 
-/*1.forEach***********
+//1.forEach***********
+/*
 const array = [1,2,3,4];
 for (let i = 0; i < array.length; i++) {
    console.log(array[i]); 
@@ -18,9 +19,11 @@ array.forEach((value)=>{
 });
 
 console.log(array);
+*/
 
-/*2.Map ***********
+//2.Map ***********
 // 값이 3개 => 리턴 변경된 3개 값이 나와야됨.
+/*
 const array = [1,2,3,4];
 const newArray = []; 
 
@@ -34,7 +37,7 @@ console.log(newArray);
 
 let _newArray = array.map((value)=> value * 10 ); 
 console.log(_newArray); 
-
+*/
 
 
 /*3. filter 
@@ -57,7 +60,7 @@ console.log(newArray1);
 
 let newArray2 = array1.filter((value)=> value.hobby === "테니스"); 
 console.log(newArray2); 
-
+*/
 /*4. find, findIndex 
 let array1 = [ 
 { name: "구길동", hobby: "테니스" }, 
@@ -81,7 +84,7 @@ let findIndex2 = array1.findIndex((value)=> value.hobby === "독서");
 
 console.log(findIndex2); 
 
-
+*/
 /*5.slice(start, end)
 let array1 = [ 
 { name: "구길동", hobby: "테니스" }, 
@@ -94,7 +97,7 @@ let array1 = [
 
 let newArray = array1.slice(0,3);
 console.log(newArray);
-
+*/
 /*6. concat 
 let array1 = [ 
 { name: "구길동", hobby: "테니스" }, 
@@ -112,7 +115,7 @@ let array3 = array1.concat(array2);
 console.log(array3);
 
 
-/* 7.sort, sort().reverse()
+//* 7.sort, sort().reverse()
 // 배열을 사전순으로 내림차순 정렬하는 메서드 
 // 자바스크립트는 문자단위 정렬
 //숫자단위는 정렬은 문제가발생한다.
---
+++ section01/chapter01/chapter23.js
@@ -0,0 +1,7 @@
+let person = { 
+  name: "홍길동", 
+  age: 27, 
+  hobby: "테니스", 
+}; 
+
+console.log(JSON.stringify(person))
